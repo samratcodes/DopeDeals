2f934de574b670f6d2aa31aa51d9314dd78e1027
SamratCodess <samratcodess@gmail.com>
2024-01-14 17:51:27 +0545

    navbar

--- DopeDeals/src/Components/Navigation/NavigationMenu.jsx
+++ DopeDeals/src/Components/Navigation/NavigationMenu.jsx
@@ -4,31 +4,32 @@ const NavigationMenu = () => {
   return (
     <>
     <div 
-    className='flex w-full justify-between items-center'
+    className='flex w-full justify-between pt-2 items-center'
     id='NavTopSection'
     >
     <div 
     className='flex  w-1/6 flex-col justify-center items-center'
     >
     <img 
-    className=' w-16 rounded-lg' 
+    className=' w-10 rounded-lg' 
     src="src\assets\Logo.png" alt=""
     />
-    <h1 className=' text-base text-gray-400 font-semibold font-mono'>
+    <h1 className=' text-base text-green-500 font-semibold font-mono'>
         Dope Deals
     </h1>
     </div>
     <div 
-    className='flex w-96 h-12 rounded-md p-2 bg-white' 
+    className='flex w-96 rounded-full p-2 bg-white' 
     id='SearchSection'
     >
         <input 
         className=' w-full bg-white focus:outline-none' 
         type="text" 
+        placeholder='Search some dope shit...'
         name="" 
         id=""
          />
-        <button
+        <button 
          className=' bg-white ' 
          type="submit"
          >
@@ -39,10 +40,12 @@ const NavigationMenu = () => {
     className='flex w-1/6 justify-around items-center'
     id='NavDetailsSection'
     >
-     <i className="fa-solid fa-question"></i>
+      <div className='flex flex-col justify-center items-center'>
+      <i className="fa-solid fa-question"></i>
+      </div>
      <i className="fa-regular fa-bell"></i>
      <i className="fa-solid fa-cart-shopping"></i>
-     <i className=" fa-solid fa-user"></i>
+     <img src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
     </div>
     </div>
     </>
--- DopeDeals/src/Components/Navigation/TopBar.jsx
+++ DopeDeals/src/Components/Navigation/TopBar.jsx
@@ -2,8 +2,36 @@ import React from 'react'
 
 const TopBar = () => {
   return (
+    <div className='w-1/2  p-5 flex justify-around items-center text-white text-base font-semibold '>
     <div>
-cxvxcvcx
+      Home 
+    </div>
+    <div class=" group">
+  <div >
+    Shop <i class="fa-solid fa-caret-down  group-hover:rotate-180"></i>
+  </div>
+  
+
+  <div class="hidden absolute mt-0 bg-white border border-green-600 py-2 px-4 rounded-lg group-hover:block">
+ 
+    <a href="#" class="block hover:text-blue-500">Submenu Item 1</a>
+    <a href="#" class="block hover:text-blue-500">Submenu Item 2</a>
+   
+  </div>
+</div>
+    {/* <div >
+    Shop <i class="fa-solid fa-caret-down transform origin-center transition-transform hover:rotate-90"></i>
+
+    </div> */}
+    <div>
+     Categories <i className="fa-solid fa-caret-down"></i>
+    </div>
+    <div>
+      Dopest Deals <i className="fa-solid fa-caret-down"></i>
+    </div>
+    <div>
+      Blogs
+    </div>
     </div>
   )
 }
