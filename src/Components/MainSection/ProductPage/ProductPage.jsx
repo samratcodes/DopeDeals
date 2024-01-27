a88ce0a93a36b9a17135295e292235640b07d8bf
SamratCodess <samratcodess@gmail.com>
2024-01-27 09:21:34 +0545

    Product Page changes

--- src/Components/MainSection/ProductPage/ProductPage.jsx
+++ src/Components/MainSection/ProductPage/ProductPage.jsx
@@ -16,15 +16,13 @@ const ProductPage = () => {
                                                       brand:"Apple",
                                                       category:"smartphones",
                                                       thumbnail:"https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
-                                                      images:["https://cdn.dummyjson.com/product-images/1/1.jpg",
-                                                      "https://cdn.dummyjson.com/product-images/1/2.jpg",
-                                                      "https://cdn.dummyjson.com/product-images/1/3.jpg",
-                                                      "https://cdn.dummyjson.com/product-images/1/4.jpg",
-                                                      "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"]}
+                                                      images:[
+                                                      ""]}
                                                         )
       useEffect(() => {
       if(!loading){
         setActiveProduct(results)
+        setPrice((results.price - (results.price * results.discountPercentage) / 100).toFixed(2))
         setActiveImage(results.images[0])
       }
     }, [loading]);                                                      
@@ -37,7 +35,7 @@ const changeActiveImage=(image)=>{
    < div className='flex flex-col items-center p-4' id='ProductPage'>
 {loading?<div>Loading...</div>:
  
- <div   className='w-4/5  flex  flex-col sm:flex-row'>
+ <div   className=' w-11/12 sm:w-4/5  flex  flex-col sm:flex-row'>
  <div className='w-full sm:w-1/2 border-r-2 border-slate-200'>
     
 <div className='w-full '>
@@ -46,7 +44,7 @@ const changeActiveImage=(image)=>{
     </div>
     <div className='flex justify-between h-14 sm:h-20' id='imageOptions'>
 {activeProduct.images.map((image, index) => (
-<img key={index} onClick={()=>changeActiveImage(image)} className={`h-full border-4 ${activeImage===image?'border-green-500':'border-white'} `} src={image} alt="" style={{ width: `${100 / activeProduct.images.length}%` }} />
+<img key={index} onClick={()=>changeActiveImage(image)} className={`h-full border-4 bg-white object-contain ${activeImage===image?'border-green-500':'border-white'} `} src={image} alt="" style={{ width: `${100 / activeProduct.images.length}%` }} />
 ))}
 </div>
 
@@ -55,17 +53,17 @@ const changeActiveImage=(image)=>{
 </div>
 <div className='w-full p-2 sm:w-1/2 bg-white flex flex-col justify-around '>
 <div className='flex flex-col justify-center items-center'>
-<h1 className=' text-2xl text-green-500 font-bold'>{activeProduct.title}</h1>
-<h1 className=' text-lg'>{activeProduct.description}</h1>
+<h1 className='text-lg sm:text-2xl text-green-500 font-bold'>{activeProduct.title}</h1>
+<h1 className=' text-sm sm:text-lg'>{activeProduct.description}</h1>
 </div>
 <div className=' flex items-end justify-center'>
-<h1 className=' text-3xl font-bold' >{Price}</h1> &nbsp;
+<h1 className=' text-2xl sm:text-4xl font-bold' >${Price}</h1> &nbsp;
 <div className=' line-through text-lg '>${activeProduct.price}</div>
 <div className=' text-red-500'>-{activeProduct.discountPercentage}%</div>
 </div>
 <div className=' flex flex-col items-center justify-center text-lg'>
 
-<div className='flex items-center text-yellow-500 text-xl font-bold'>
+<div className='flex items-center text-yellow-500 text-2xl font-bold'>
         {Array.from({ length: activeProduct.rating }, (_, index) => (
         <i key={index} className="fas fa-star"></i>))}
         {activeProduct.rating % 1 > 0.25 && <i className="fas fa-star-half-alt"></i>}
@@ -73,7 +71,7 @@ const changeActiveImage=(image)=>{
 
 </div>
 <div>Category: <Link to='/Category' className=' text-blue-400'> {activeProduct.category}</Link></div>
-<div className='text-2xl font-semibold'>Brand:  {activeProduct.brand}</div>
+<div className=' text-base sm:text-2xl font-semibold'>Brand:  {activeProduct.brand}</div>
 <div className=' text-green-500'>Stocks: {activeProduct.stock}</div>
 </div>
 <div className='w-full'>
